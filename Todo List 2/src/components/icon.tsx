e76fd5a9b79a1231b3655099859710dd81d6d080
Luiz Fernando Vital <luizfvital@gmail.com>
2025-10-30 23:42:02 +0000

    Feat: add cva to icon component

--- Todo List 2/src/components/icon.tsx
+++ Todo List 2/src/components/icon.tsx
@@ -1,9 +1,24 @@
 import type React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface IconProps extends React.ComponentProps<"svg"> {
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      false: "",
+      true: "animate-spin",
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
+
+interface IconProps
+  extends React.ComponentProps<"svg">,
+    VariantProps<typeof iconVariants> {
   svg: React.FC<React.ComponentProps<"svg">>;
 }
 
-export default function Icon({ svg: SvgComponent, ...props }: IconProps) {
-  return <SvgComponent {...props} />;
+export default function Icon({ svg: SvgComponent, animate, className ...props }: IconProps) {
+  return <SvgComponent className={iconVariants({animate, className})} {...props} />;
 }
