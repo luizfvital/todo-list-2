5779b93627429ca3f711f05e862cfde09fc1f88c
Luiz Fernando Vital <luizfvital@gmail.com>
2025-10-30 23:25:40 +0000

    Feat: add icon component

--- Todo List 2/src/components/icon.tsx
+++ Todo List 2/src/components/icon.tsx
@@ -1,1 +1,9 @@
-export default function Icon() {}
+import type React from "react";
+
+interface IconProps extends React.ComponentProps<"svg"> {
+  svg: React.FC<React.ComponentProps<"svg">>;
+}
+
+export default function Icon({ svg: SvgComponent, ...props }: IconProps) {
+  return <SvgComponent {...props} />;
+}
